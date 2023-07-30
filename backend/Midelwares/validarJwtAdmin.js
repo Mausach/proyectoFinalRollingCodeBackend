766e121bc5e9e68702c2fb64ce8ed9d09897d82b
Mauro Ybañez <maurogabriely@gmail.com>
2023-07-29 21:09:43 -0300

    protegiendo rutas desde el back

---
+++ backend/Midelwares/validarJwtAdmin.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+
+const validarJWTAdmin = (req, res, next) => {
+    //x-token headers
+    const token = req.header('x-token');
+
+    if (!token) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'No hay token en la peticion',
+        });
+    }
+
+    try {
+        const payload = jwt.verify(token, process.env.SECRET_JWT);
+        if (payload.usuario === 'usuario') {
+            return res.status(404).json({
+                ok: false,
+                msg: 'usuario no tiene rol admin',
+            });
+        }
+
+        req.id = payload.id;
+        req.name = payload.name;
+        req.rol = payload.usuario;
+    } catch (error) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'token no valido',
+        });
+    }
+
+    next();
+};
+
+module.exports = {
+    validarJWTAdmin,
+};
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -3,13 +3,14 @@ const { crearProducto, editarProducto, eliminarProducto, cargarProducto, cargarU
 
 const { check } = require('express-validator');
 const { validarCampos } = require('../Midelwares/validarCampos');
-const { validarJWT } = require('../Midelwares/validarJwt');
+
+const { validarJWTAdmin } = require('../Midelwares/validarJwtAdmin');
 
 const routerAdmin = express.Router();
 
 
-routerAdmin.post(
-  '/new', validarJWT, [
+routerAdmin.post( 
+  '/new', validarJWTAdmin, [ //la ruta para crear un nuevo producto
 
   check("name", "el nombre es obligatorio").not().isEmpty(),
   check("precio", "el precio es obligatorio").not().isEmpty(),
@@ -26,7 +27,7 @@ routerAdmin.post(
 
 
 routerAdmin.put(
-  '/edit', validarJWT, [ //la ruta seria /admin/edit para editar productos
+  '/edit', validarJWTAdmin, [ //la ruta seria /admin/edit para editar productos
 
   check("name", "el nombre es obligatorio").not().isEmpty(),
   check("precio", "el precio es obligatorio").not().isEmpty(),
@@ -40,19 +41,19 @@ routerAdmin.put(
 ], editarProducto
 );
 
-routerAdmin.delete('/eliminar/:id', validarJWT, eliminarProducto);
+routerAdmin.delete('/eliminar/:id', validarJWTAdmin, eliminarProducto);
 
 routerAdmin.get('/productos', cargarProducto);//no lleva validar token asi los usuarios no logueados puedan ver la tienda
 
 routerAdmin.get('/productos/aleatorios', cargarProducto_Aleatorio);
 
-routerAdmin.get('/usuarios', validarJWT, cargarUsuarios);
+routerAdmin.get('/usuarios', validarJWTAdmin, cargarUsuarios);
 
-routerAdmin.get('/pedidos', validarJWT, cargarPedidos);
+routerAdmin.get('/pedidos', validarJWTAdmin, cargarPedidos);
 
-routerAdmin.put('/confirmar', validarJWT, confirmarPedido);
+routerAdmin.put('/confirmar', validarJWTAdmin, confirmarPedido);
 
-routerAdmin.put('/Deshabilitar', validarJWT, inhabilitarUsuario);
+routerAdmin.put('/Deshabilitar', validarJWTAdmin, inhabilitarUsuario);
 
 
 //aclaras que se exporta todo lo trabajado con router
